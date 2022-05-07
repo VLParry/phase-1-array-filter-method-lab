d847ec91b00db8fd621fa89484ebd4a34cd3230b
Victoria Parry <vlparry@gmail.com>
2022-05-07 13:43:03 -0600

    commit tree adac81c86cfa5cac3d332a1218fea910d33c7fee

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(function (element) {
+        return element.toLowerCase().indexOf(string.toLowerCase()) !== -1
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function (element) {
+        return element.startsWith(string)
+    })
+}
+
+function matchName(object, string) {
+    return object.filter(driver => driver.name === string)
+}
